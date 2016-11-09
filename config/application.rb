6b2697effa6700c4d3fb8b2cc0f3c8cd4fee4e5d
nishina <n.toshi.1.014@gmail.com>
2016-11-10 00:05:19 +0900

    Add auto load setting

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module GrabeJbuilderSample
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.paths.add File.join('app', 'apis'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'apis', '*')]
   end
 end
